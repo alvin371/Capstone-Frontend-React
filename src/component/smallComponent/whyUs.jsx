f1100e2d4ce5feba02867d683de125199bd7a778
barcodelou <yonathanverrel@student.telkomuniversity.ac.id>
2021-12-21 20:35:53 +0700

    add fitur why us

--- src/component/smallComponent/whyUs.jsx
+++ src/component/smallComponent/whyUs.jsx
@@ -2,10 +2,85 @@ import React from "react";
 
 const whyUs = () => {
   return (
-    <div class="flex justify-center bg-gray">
-      <div class="text-gray-700 text-center bg-gray-dark px-4 py-2 m-2">1</div>
-      <div class="text-gray-700 text-center bg-gray-dark px-4 py-2 m-2">2</div>
-      <div class="text-gray-700 text-center bg-gray-dark px-4 py-2 m-2">3</div>
+    <div class="xl:flex justify-center space-x-9 p-4 m-auto bg-gray h-auto  md:block md:align-middle">
+      <div class="max-w-md mx-auto my-2 bg-white shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+          <div class="md:flex-shrink-0">
+            <img
+              class="h-48 w-full object-cover md:h-full md:w-48"
+              src="/img/store.jpg"
+              alt="Man looking at item at a store"
+            />
+          </div>
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Case study
+            </div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              Finding customers for your new business
+            </a>
+            <p class="mt-2 text-gray-500">
+              Getting a new business off the ground is a lot of hard work. Here
+              are five ideas you can use to find your first customers.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="max-w-md mx-auto my-2 bg-white  shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+          <div class="md:flex-shrink-0">
+            <img
+              class="h-48 w-full object-cover md:h-full md:w-48"
+              src="/img/store.jpg"
+              alt="Man looking at item at a store"
+            />
+          </div>
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Case study
+            </div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              Finding customers for your new business
+            </a>
+            <p class="mt-2 text-gray-500">
+              Getting a new business off the ground is a lot of hard work. Here
+              are five ideas you can use to find your first customers.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="max-w-md mx-auto my-2 bg-white shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+          <div class="md:flex-shrink-0">
+            <img
+              class="h-48 w-full object-cover md:h-full md:w-48"
+              src="/img/store.jpg"
+              alt="Man looking at item at a store"
+            />
+          </div>
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Case study
+            </div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              Finding customers for your new business
+            </a>
+            <p class="mt-2 text-gray-500">
+              Getting a new business off the ground is a lot of hard work. Here
+              are five ideas you can use to find your first customers.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
