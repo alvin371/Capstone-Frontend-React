041d957d1a758ce4312d435b99a00b094687dc55
barcodelou <yonathanverrel@student.telkomuniversity.ac.id>
2022-01-09 22:21:37 +0700

    fixin homepage

--- src/component/smallComponent/dropdownNews.jsx
+++ src/component/smallComponent/dropdownNews.jsx
@@ -41,7 +41,7 @@ export default function Example() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-top-right right-0 mt-2 w-56 bg-white ">
+        <Menu.Items className="origin-top-right  right-0 mt-2 w-56 bg-white ">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
--- src/page/homepage/index.js
+++ src/page/homepage/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../component/pageComponent/Navbar/Navbar";
+import Footer from "../../component/smallComponent/footer";
+import FitnessLogo from "../../component/asset/Header.png";
+import Stycky from "../../component/smallComponent/sticky";
+import WhyUs from "../../component/smallComponent/whyUs";
+import About from "../../component/smallComponent/about"
 
 const HomePage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="bg-black h-full space-y-6">
+      <Navbar />
+      <img
+        src={FitnessLogo}
+        alt="head"
+        className="mx-auto w-2/5 opacity-70 mb-10 sm:w-1/2"
+      />
+      <div className="fixed top-1/4 mr-4 right-0 opacity-50">
+        <Stycky />
+      </div>
+      <h1 className="relative text-gray font-bold text-center text-4xl mb-8">
+        WHY US
+      </h1>
+      <WhyUs/>
+      <h1 className="relative text-gray font-bold text-center text-4xl mb-8">
+        ABOUT US
+      </h1>
+      <About/>
+      <Footer />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
